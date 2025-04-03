5fe1124e4c3721b68c953bf1d269382782168cdf
Albert <albert8moulin@gmail.com>
2025-04-03 12:20:07 +0200

    Started working on agent file, and added a function that combines everything from the FindClosestTown file

--- FindClosestTown.py
+++ FindClosestTown.py
@@ -2,6 +2,8 @@ from functools import partial
 from geopy.geocoders import Nominatim
 import pandas as pd
 import folium
+import numpy as np
+import pdb
 
 class FindClosestTown():
 
@@ -16,37 +18,48 @@ class FindClosestTown():
 
     def find_closest_town_hall(self):
         town_hall_data = pd.read_csv("data_mairies.csv")
-        min_dist2 = 1000
-        for latitude, longitude in zip(town_hall_data['latitude'],town_hall_data['longitude']):
+        min_dist2 = np.inf
+        for i, (latitude, longitude) in enumerate(zip(town_hall_data['latitude'],town_hall_data['longitude'])):
             if (latitude-self.location.latitude)**2 + (longitude-self.location.longitude)**2 < min_dist2:
                 min_dist2 = (latitude-self.location.latitude)**2 + (longitude-self.location.longitude)**2
                 minlat = latitude
                 minlong = longitude
-        return self.reverse(str(minlat) + "," + str(minlong))
+                index = i
+        return (self.reverse(str(minlat) + "," + str(minlong)), town_hall_data.iloc[index])
 
-if __name__ == "__main__":
-    fct = FindClosestTown("Hardelot Neufchatel")
-    cth = fct.find_closest_town_hall()
-    print(cth)
-    m = folium.Map((fct.location.latitude,fct.location.longitude), tiles="cartodb positron")
-    folium.Marker(
-        location=[cth.latitude, cth.longitude],
-        tooltip="Click me!",
-        popup="Town Hall",
-        icon=folium.Icon(color="green"),
-    ).add_to(m)
-    folium.Marker(
-        location=[fct.location.latitude, fct.location.longitude],
-        tooltip="Click me!",
-        popup="Adresse",
-        icon=folium.Icon(color="red"),
-    ).add_to(m)
-    folium.Marker(
-        location=[50.619594, 1.643270],
-        tooltip="Click me!",
-        popup="Adresse",
-        icon=folium.Icon(color="blue"),
-    ).add_to(m)
-    m.save("map.html")
+    def find_print_closest_hall(adress : str):
+        """
+        This function identifies the closest town hall to a given address.
+
+        Function details:
+        - **Input**: A string representing the address.
+        - **Output**:
+            1. **Printed Location**: The address of the closest town hall.
+            2. **Saved Map**: A visual map displaying both the input address and the closest town hall.
+            3. **Return Values**: 
+                - A `geopy.location.Location` object representing the town hall's coordinates.
+                - A Pandas Series containing all relevant details about the town hall.
+        """
+        fct = FindClosestTown(adress)
+        cth, column = fct.find_closest_town_hall()
+        print("Your adress :", fct.location)
+        print("The closest town hall", cth)
+        legende_town_hall : str = "\n".join([column['nom'], column['adresse_courriel'], column['website']])
+        m = folium.Map((fct.location.latitude,fct.location.longitude), tiles="cartodb positron")
+        folium.Marker(
+            location=[cth.latitude, cth.longitude],
+            tooltip="Mairie",
+            popup=legende_town_hall,
+            icon=folium.Icon(color="green"),
+        ).add_to(m)
+
+        folium.Marker(
+            location=[fct.location.latitude, fct.location.longitude],
+            tooltip="Votre adresse",
+            popup=fct.adress,
+            icon=folium.Icon(color="red"),
+        ).add_to(m)
+        m.save("map.html")
+        return (cth, column)
 
     
---
+++ agent.py
@@ -0,0 +1,3 @@
+from FindClosestTown import FindClosestTown
+
+FindClosestTown.find_print_closest_hall("93 bis avenue achille peretti")
